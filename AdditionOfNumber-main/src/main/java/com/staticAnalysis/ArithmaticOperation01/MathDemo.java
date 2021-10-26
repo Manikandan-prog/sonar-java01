a75907d90630996e0deacfb8e9166c13c80c1303
Manikandan-prog <84707867+Manikandan-prog@users.noreply.github.com>
2021-10-26 15:26:37 +0530

    Add files via upload

---
+++ AdditionOfNumber-main/src/main/java/com/staticAnalysis/ArithmaticOperation01/MathDemo.java
@@ -0,0 +1,15 @@
+package com.staticAnalysis.ArithmaticOperation01;
+
+public class MathDemo {
+	public int add(int a,int b){
+		return a+b;
+		}
+		 
+		public int subtract(int a,int b){
+		return a-b;
+		}
+		public static void main(String[] args) {
+			
+		}
+		
+}
